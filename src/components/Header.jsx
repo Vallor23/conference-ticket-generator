5c0c98a7446d5e521666d2171f08a45899ec003a
Vallor23 <enosnyabinda2017@gmail.com>
2025-02-16 21:05:08 +0300

    Modified Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,19 @@ import thumb from '../assets/thumb.png'
 import ticz from '../assets/ticz.png'
 
 import { FaArrowRight } from "react-icons/fa";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 const Header = () => {
     return (
-        <header className="bg-[#05252C] m-4 flex flex-row justify-between items-center px-4 py-4 border-1 border-[#197686] border-solid rounded-2xl">
+        <header className="bg-[#05252C] m-4 flex flex-row justify-between items-center px-4 py-4 border-solid rounded-2xl w-full  mx-auto  border-[#197686] max-w-[1200px] min-w-[320px]border backdrop-blur-[2px] mb-12">
             <div className='flex items-center justify-between gap-2'>
                 <img src={thumb} alt="" />
                 <img src={ticz} alt="" />
             </div>
-            <div className='hidden'>
-                <p>Events</p>
-                <p>Tickets</p>
-                <p>About project</p>
+            <div className=' items-center hidden text-lg gap-x-4 lg:flex' style={{fontFamily:'NanumMyeongjo'}}>
+                <p className='text-white'>Events</p>
+                <p className='text-[#b3b3b3]'>Tickets</p>
+                <p className='text-[#b3b3b3]'>About project</p>
             </div>
             <button className='flex justify-between items-center bg-white text-[#0A0C11] px-4 py-3 rounded-2xl gap-2'>
 
